214f7441993ebff772ecd0c235645dff340204a1
Mikołaj Chyliński <mixerofixero7@gmail.com>
2026-04-13 20:10:11 +0200

    new files

--- app.py
+++ app.py
@@ -1,50 +1,71 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, g
+import sqlite3
 
 app = Flask(__name__)
+DATABASE = "todo.db"
+SCHEMA_SQL = """
+CREATE TABLE IF NOT EXISTS tasks(
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+title TEXT NOT NULL,
+done INTEGER NOT NULL DEFAULT 0 CHECK (done IN (0,1)),
+created_at TEXT NOT NULL DEFAULT (datetime('now'))
+);
+
+CREATE INDEX IF NOT EXISTS idx_tasks_done ON tasks(done);
+CREATE INDEX IF NOT EXISTS idx_tasks_created_at ON tasks(created_at);
+"""
+
+def get_db():
+    if "db" not in g:
+        conn = sqlite3.connect(DATABASE)
+        conn.row_factory = sqlite3.Row
+        conn.execute("PRAGMA foregin_keys=ON;")
+        g.db = conn
+
+    return g.db
+
+@app.teardown_appcontext
+def close_db(exception):
+    db = g.pop("db", None)
+    if db is not None:
+        db.close()
+
+def init_db():
+    db = get_db()
+    db.executescript(SCHEMA_SQL)
+    db.commit()
+
+@app.cli.command("init-db")
+def init_db_command():
+    init_db()
+    print("Zainicjowano baze danych")
+
+@app.cli.command("seed-db")
+def seed_db_command():
+    db = get_db()
+    howManyTasks = db.execute("SELECT COUNT(*) FROM tasks").fetchone()[0]
+    if howManyTasks == 0:
+        db.executemany("INSERT INTO tasks(title, done) VALUES (?,?)",[["Sniadanie",1],["Wyjsc po mleko",0],["zmywac naczynia", 0]])
+        db.commit()
+
+        print("Dane przykladowe zostaly dodane do tabeli tasks.")
+    else:
+        print("X Tabela tasks zawiera juz dane, seedowanie przerwane.")
+
+
+
+@app.route("/ping_db")
+def ping_db():
+    db = get_db()
+    db.execute("SELECT 1").fetchone()
+    return render_template("ping.html")
+
+
 
 @app.route("/")
 def index():
-    return "flask dziala"
+    return render_template("index.html")
 
-@app.route("/form", methods=["GET", "POST"])
-def form():
-    fullname = None
-    error = None
-    if request.method == "POST":
-        fullname = request.form.get('fullname')
-        if not fullname.strip():
-            error = "Nie podales swojego imienia i nazwiska"
-            return render_template("form.html", fullname = None, erorr = error)
+if __name__ == "__main__":
 
-    return render_template("form.html", fullname = fullname)
-
-@app.route("/better_calc", methods=["GET", "POST"])
-def calc():
-    result = None
-    error = None
-    a = None
-    b = None
-    operator = None
-    if request.method == "POST":
-        a = request.form.get('a')
-        b = request.form.get('b')
-        operator = request.form.get('operator')
-
-        if not a or not b:
-            error = "nie podales liczb"
-            return render_template("better_calc.html", result = None, error=error, a = a,
-    b = b,
-    operator = operator)
-        
-        if operator == "/" and "b" == "0":
-            return render_template("better_calc.html", result = None, error=error, a = a,
-    b = b,
-    operator = operator)
-        
-        
-        result = eval(f"{a}{operator}{b}")
-
-    return render_template("better_calc.html", result=result, error=error, a=a, b=b, operator = operator)
-
-
-app.run(debug=True)
+    app.run(debug=True)
